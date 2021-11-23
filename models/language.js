48867da1ec80a5b9c122062dddcf7c7ad08f5711
jmeier2715 <jmeier2715@gmail.com>
2021-11-23 12:32:17 -0500

    ready for deployment

--- models/language.js
+++ models/language.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      models.language.belongsTo(models.user)
+      models.language.belongsTo(models.textSubmission)
     }
   };
   language.init({
